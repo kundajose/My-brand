99c63716787c16f388c0b1066195d0ab5ff2da81
IRADUKUNDA Joseph <iradukundajose8@gmail.com>
2022-11-25 15:07:25 +0200

    adding dashboard

---
+++ addmin.js
@@ -0,0 +1,60 @@
+document.querySelector('#update').addEventListener('click',join)
+
+let names= document.querySelector('li')
+
+let data= document.querySelector('td')
+
+function join(){
+  let firstN=document.querySelector('#name').value
+  let firstD=document.querySelector('#date').value
+  names.innerHTML +=firstN
+  data.innerHTML += firstD
+
+}
+// open and close dashbboard
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+//  Messages
+function myAddmin() {
+  var x = document.getElementById("contact");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function myAddmin() {
+  var x = document.getElementById("contact");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function myComment() {
+  var x = document.getElementById("user-comment");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+// sidebar
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
---
+++ blog.js
@@ -0,0 +1,8 @@
+function myComment() {
+    var x = document.getElementById("user-comment");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
